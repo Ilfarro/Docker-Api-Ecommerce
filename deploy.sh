fa6ba158f06a765d7f84537c7f9711c05177785b
Ilham <ilham_farrohi@yahoo.com>
2019-03-20 16:50:05 +0700

    please bisa

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ source ~/.profile
 echo "$DOCKERHUB_PASS" | sudo docker login --username $DOCKERHUB_USER --password-stdin
 sudo docker stop numberforth
 sudo docker rm numberforth
-sudo docker rmi ilfarro/forth
-sudo docker run -d --name numberforth -p 5000:5000 ilfarro/forth:latest
+sudo docker rmi ilfarro/numberforth
+sudo docker run -d --name numberforth -p 5000:5000 ilfarro/numberforth:latest
