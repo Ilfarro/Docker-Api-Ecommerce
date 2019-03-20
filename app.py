1218165633de3ce7b4a042397c78c39a46d9b6e4
Ilham <ilham_farrohi@yahoo.com>
2019-03-20 11:58:22 +0700

    ganti port lagi

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ if __name__ == '__main__':
         else:
             app.run(debug=True, host='0.0.0.0', port=5000)
     except IndexError as e:
-        app.run(debug=True, host='0.0.0.0', port=500)0
+        app.run(debug=True, host='0.0.0.0', port=5000)
