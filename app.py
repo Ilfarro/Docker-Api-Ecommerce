854f032f09059c11898853b20e7db4d2d904fe95
Ilham <ilham_farrohi@yahoo.com>
2019-03-20 11:55:20 +0700

    ganti port

--- app.py
+++ app.py
@@ -17,6 +17,6 @@ if __name__ == '__main__':
         if sys.argv[1] == 'db':
             manager.run()
         else:
-            app.run(debug=True, host='0.0.0.0', port=5001)
+            app.run(debug=True, host='0.0.0.0', port=5000)
     except IndexError as e:
-        app.run(debug=True, host='0.0.0.0', port=5001)
+        app.run(debug=True, host='0.0.0.0', port=500)0
